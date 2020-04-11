ee3d8ff686bc163e551283da37b5b96de9b08510
NotPixel <61543409+NotPixel@users.noreply.github.com>
2020-04-11 20:56:19 +0100

    Uploading the resource manifest.

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+game 'gta5'
+description 'NotPixel - Robbery Cooldown'
+version '1.0.0'
+
+server_scripts {
+    'server/server.lua'
+}
+
+server_export 'StartCooldown'
+server_export 'CooldownCheck'
